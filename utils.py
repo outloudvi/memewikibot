df13f00e284f601fbd799cd75e2c61dc4449f8da
Outvi V <git@outv.im>
2020-02-23 22:30:41 +0800

    fix: Go with latest Template:JISFW formats

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import re
 
-JISFW_LINE = re.compile(r'\{\{JISFW\|id=([0-9]+)\}\}')
+JISFW_LINE = re.compile(r'\{\{JISFW(.+)\}\}')
 REDIRECT_LINE = re.compile(r'#redirect \[\[(.+)\]\]')
 
 
@@ -14,5 +14,9 @@ def parse_jisfw_text(text):
                 "redirect": REDIRECT_LINE.match(i).groups()[0]
             }
         elif JISFW_LINE.match(i):
-            tags["id"].append(JISFW_LINE.match(i).groups()[0])
+            matches = JISFW_LINE.match(i).groups()[0].split("|")
+            for i in matches:
+                target = re.sub(r".+=", "", i)
+                if target.isdigit():
+                    tags["id"].append(target)
     return tags
